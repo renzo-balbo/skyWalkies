6f8e5bae83256f9c6f82046d679db84f37d6d8b0
Renzo Balbo <renzo.balbo@outlook.com.ar>
2022-09-13 15:55:32 -0300

    The first draft of the final MindHub project.

---
+++ src/main/java/com/mindhub/skywalkies/SkywalkiesApplication.java
@@ -0,0 +1,13 @@
+package com.mindhub.skywalkies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkywalkiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkywalkiesApplication.class, args);
+	}
+
+}
