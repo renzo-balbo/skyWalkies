e60b67df4deb72d841a2ab68b154104d50ffe9d5
Renzo Balbo <renzo.balbo@outlook.com.ar>
2022-09-14 19:23:58 -0300

    El generador y customizador de avatar esta casi listo para agregar al index.

--- src/main/resources/static/assets/js/avatar.js
+++ src/main/resources/static/assets/js/avatar.js
@@ -5,17 +5,46 @@ createApp({
     data() {
         return {
             clientAvatar: {},
-            customAvatar:{}
-            
-
+            customAvatar:{},
+            avatarHead: new Image(),
+            avatarBody: new Image(),
+            avatarBodyColor: new Image(),
+            avatarFace: new Image(),
+            avatarShoes: new Image(),
+            avatarArtLine: new Image(),
         }
     },
-    created() {
-        this.loadClientData()
-        console.log(this.clientAvatar)
+    beforeCreate(){
+        console.log("before create")
     },
-    mounted() {
+    created() {
+        console.log("created")
+        this.loadClientData()
+    },
 
+    beforeMount(){
+        console.log("before mount")
+    },
+
+    mounted() {
+        this.prepareAvatarParts();
+        this.drawAvatar();
+        console.log("mounted")
+    },
+
+    beforeUpdate(){
+        this.avatarHead.src="../assets/img/avatarCollection/head"+this.customAvatar.head+".png";
+        this.avatarBody.src="../assets/img/avatarCollection/body"+this.customAvatar.body+".png";
+        this.avatarBodyColor.src="../assets/img/avatarCollection/bodyColor"+this.customAvatar.bodyColor+".png";
+        this.avatarFace.src="../assets/img/avatarCollection/face"+this.customAvatar.face+".png";
+        this.avatarShoes.src="../assets/img/avatarCollection/shoes"+this.customAvatar.shoes+".png";
+        this.avatarArtLine.src="../assets/img/avatarCollection/lineArtObligatory.png";
+        console.log("before update")
+    },
+
+    updated(){
+        // this.drawCustomAvatar();
+        console.log("updated")
     },
     methods: {
         loadClientData() {
@@ -27,7 +56,7 @@ createApp({
                 shoes: 1
             }
             this.customAvatar = this.clientAvatar;
-            // this.drawCustomAvatar()
+
 
         },
 
@@ -35,70 +64,54 @@ createApp({
         drawAvatar() {
             let canvas = document.getElementById("myCanvas");
             let ctx = canvas.getContext("2d");
-            let headImg = new Image()
-            headImg.src = "../assets/img/avatarCollection/head" + this.clientAvatar.head + ".png";
 
-            let bodyImg = new Image()
-            bodyImg.src = "../assets/img/avatarCollection/body" + this.clientAvatar.body + ".png";
-
-            let bodyColor = new Image();
-            bodyColor.src = "../assets/img/avatarCollection/bodyColor" + this.clientAvatar.bodyColor + ".png";
-
-            let face = new Image();
-            face.src = "../assets/img/avatarCollection/face" + this.clientAvatar.face + ".png";
-
-            let shoes = new Image();
-            shoes.src = "../assets/img/avatarCollection/shoes" + this.clientAvatar.shoes + ".png";
-
-            let lineArt = new Image();
-            lineArt.src = "../assets/img/avatarCollection/lineArtObligatory.png";
-
-
-            ctx.drawImage(bodyColor, 0, 0);
-            ctx.drawImage(face, 0, 0);
-            ctx.drawImage(shoes, 0, 0);
-            ctx.drawImage(bodyImg, 0, 0);
-            ctx.drawImage(lineArt, 0, 0);
-            ctx.drawImage(headImg, 0, 0);
+            ctx.drawImage(this.avatarBodyColor, 0, 0);
+            ctx.drawImage(this.avatarFace, 0, 0);
+            ctx.drawImage(this.avatarShoes, 0, 0);
+            ctx.drawImage(this.avatarBody, 0, 0);
+            ctx.drawImage(this.avatarArtLine, 0, 0);
+            ctx.drawImage(this.avatarHead, 0, 0);
         },
 
         //ESTA FUNCION VA A DIBUJAR EL AVATAR QUE ESTE MODIFICANDO EL CLIENTE
         drawCustomAvatar() {
             let canvas = document.getElementById("myCanvas");
             let ctx = canvas.getContext("2d");
+
             ctx.clearRect(0,0, canvas.width, canvas.height);
-
-            let headImg = new Image()
-            headImg.src = "../assets/img/avatarCollection/head" + this.customAvatar.head + ".png";
-
-            let bodyImg = new Image()
-            bodyImg.src = "../assets/img/avatarCollection/body" + this.customAvatar.body + ".png";
-
-            let bodyColor = new Image();
-            bodyColor.src = "../assets/img/avatarCollection/bodyColor" + this.customAvatar.bodyColor + ".png";
-
-            let face = new Image();
-            face.src = "../assets/img/avatarCollection/face" + this.customAvatar.face + ".png";
-
-            let shoes = new Image();
-            shoes.src = "../assets/img/avatarCollection/shoes" + this.customAvatar.shoes + ".png";
-
-            let lineArt = new Image();
-            lineArt.src = "../assets/img/avatarCollection/lineArtObligatory.png";
             
-            ctx.drawImage(bodyColor, 0, 0);
-            ctx.drawImage(face, 0, 0);
-            ctx.drawImage(shoes, 0, 0);
-            ctx.drawImage(bodyImg, 0, 0);
-            ctx.drawImage(lineArt, 0, 0);
-            ctx.drawImage(headImg, 0, 0);
+            ctx.drawImage(this.avatarBodyColor, 0, 0);
+            ctx.drawImage(this.avatarFace, 0, 0);
+            ctx.drawImage(this.avatarShoes, 0, 0);
+            ctx.drawImage(this.avatarBody, 0, 0);
+            ctx.drawImage(this.avatarArtLine, 0, 0);
+            ctx.drawImage(this.avatarHead, 0, 0);
         },
 
+
+        prepareAvatarParts(){
+            this.avatarHead.src="../assets/img/avatarCollection/head"+this.clientAvatar.head+".png";
+            this.avatarBody.src="../assets/img/avatarCollection/body"+this.clientAvatar.body+".png";
+            this.avatarBodyColor.src="../assets/img/avatarCollection/bodyColor"+this.clientAvatar.bodyColor+".png";
+            this.avatarFace.src="../assets/img/avatarCollection/face"+this.clientAvatar.face+".png";
+            this.avatarShoes.src="../assets/img/avatarCollection/shoes"+this.clientAvatar.shoes+".png";
+            this.avatarArtLine.src="../assets/img/avatarCollection/lineArtObligatory.png";
+        },
+
+        updateAvatarParts(){
+            this.avatarHead.src="../assets/img/avatarCollection/head"+this.customAvatar.head+".png";
+            this.avatarBody.src="../assets/img/avatarCollection/body"+this.customAvatar.body+".png";
+            this.avatarBodyColor.src="../assets/img/avatarCollection/bodyColor"+this.customAvatar.bodyColor+".png";
+            this.avatarFace.src="../assets/img/avatarCollection/face"+this.customAvatar.face+".png";
+            this.avatarShoes.src="../assets/img/avatarCollection/shoes"+this.customAvatar.shoes+".png";
+            this.avatarArtLine.src="../assets/img/avatarCollection/lineArtObligatory.png";
+        },
 
 
 
     },
     computed: {
+
 
     },
 }).mount('#app')
