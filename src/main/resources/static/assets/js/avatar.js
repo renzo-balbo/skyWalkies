cc8ce856c8530a0328e39a02ea9a750494ce933c
Renzo Balbo <renzo.balbo@outlook.com.ar>
2022-09-14 16:29:04 -0300

    Trabjando en avatar.html y avatar.js. Importando nuevos avatars

--- src/main/resources/static/assets/js/avatar.js
+++ src/main/resources/static/assets/js/avatar.js
@@ -1,77 +1,104 @@
-// const { createApp } = Vue
+const { createApp } = Vue
 
 
-// createApp({
-//     data() {
-//         return {
-//             avatarHead:new Image(),
+createApp({
+    data() {
+        return {
+            clientAvatar: {},
+            customAvatar:{}
+            
 
-//         }
-//     },
-//     created() {
-//         // this.loadAvatarData();
-//         // this.drawCanvas(this.avatarHead)
-//     },
-//     mounted() {
+        }
+    },
+    created() {
+        this.loadClientData()
+        console.log(this.clientAvatar)
+    },
+    mounted() {
 
-//     },
-//     methods: {
+    },
+    methods: {
+        loadClientData() {
+            this.clientAvatar = {
+                head: 3,
+                body: 4,
+                bodyColor: 2,
+                face: 2,
+                shoes: 1
+            }
+            this.customAvatar = this.clientAvatar;
+            // this.drawCustomAvatar()
 
-//         loadAvatarData(){
-//             var avatarHead = new Image()
-//             let avatarHeadName = "head3.png"
-//             avatarHead.src = "../assets/img/avatarCollection/"+avatarHeadName;
-//             this.avatarHead.src="../assets/img/avatarCollection/head3.png";
-//         },
+        },
 
-//         drawCanvas(headToLoad){
-//             let canvas = document.getElementById("myCanvas");
-//             let ctx = canvas.getContext("2d");
-//             let head = headToLoad;
-//             ctx.drawImage(head, 10, 10);
-//         }
+        //ESTA FUNCION ES PARA CARGAR EL AVATAR QUE YA TIENE EL USUARIO CARGADO DESDE EL BACK
+        drawAvatar() {
+            let canvas = document.getElementById("myCanvas");
+            let ctx = canvas.getContext("2d");
+            let headImg = new Image()
+            headImg.src = "../assets/img/avatarCollection/head" + this.clientAvatar.head + ".png";
 
-        
-//     },
-//     computed: {
+            let bodyImg = new Image()
+            bodyImg.src = "../assets/img/avatarCollection/body" + this.clientAvatar.body + ".png";
 
-//     },
-// }).mount('#app')
+            let bodyColor = new Image();
+            bodyColor.src = "../assets/img/avatarCollection/bodyColor" + this.clientAvatar.bodyColor + ".png";
 
-let avatar= {
-    head:3,
-    body:4,
-    bodyColor:2,
-    face:2,
-    shoes:1,
-}
+            let face = new Image();
+            face.src = "../assets/img/avatarCollection/face" + this.clientAvatar.face + ".png";
 
-function myCanvas() {
-    let c = document.getElementById("myCanvas");
-    let ctx = c.getContext("2d");
-    let headImg = new Image()
-    headImg.src = "../assets/img/avatarCollection/head"+avatar.head+".png";
-    
-    let bodyImg = new Image()
-    bodyImg.src="../assets/img/avatarCollection/body"+avatar.body+".png";
+            let shoes = new Image();
+            shoes.src = "../assets/img/avatarCollection/shoes" + this.clientAvatar.shoes + ".png";
 
-    let bodyColor = new Image();
-    bodyColor.src="../assets/img/avatarCollection/bodyColor"+avatar.bodyColor+".png";
-
-    let face = new Image();
-    face.src="../assets/img/avatarCollection/face"+avatar.face+".png";
-
-    let shoes= new Image();
-    shoes.src="../assets/img/avatarCollection/shoes"+avatar.shoes+".png";
-
-    let lineArt = new Image();
-    lineArt.src="../assets/img/avatarCollection/lineArtObligatory.png";
+            let lineArt = new Image();
+            lineArt.src = "../assets/img/avatarCollection/lineArtObligatory.png";
 
 
-    ctx.drawImage(bodyColor,0,0);
-    ctx.drawImage(face,0,0);
-    ctx.drawImage(shoes,0,0);
-    ctx.drawImage(bodyImg,0,0);
-    ctx.drawImage(lineArt,0,0);
-    ctx.drawImage(headImg,0,0);
-}
+            ctx.drawImage(bodyColor, 0, 0);
+            ctx.drawImage(face, 0, 0);
+            ctx.drawImage(shoes, 0, 0);
+            ctx.drawImage(bodyImg, 0, 0);
+            ctx.drawImage(lineArt, 0, 0);
+            ctx.drawImage(headImg, 0, 0);
+        },
+
+        //ESTA FUNCION VA A DIBUJAR EL AVATAR QUE ESTE MODIFICANDO EL CLIENTE
+        drawCustomAvatar() {
+            let canvas = document.getElementById("myCanvas");
+            let ctx = canvas.getContext("2d");
+            ctx.clearRect(0,0, canvas.width, canvas.height);
+            let headImg = new Image()
+            headImg.src = "../assets/img/avatarCollection/head" + this.customAvatar.head + ".png";
+
+            let bodyImg = new Image()
+            bodyImg.src = "../assets/img/avatarCollection/body" + this.customAvatar.body + ".png";
+
+            let bodyColor = new Image();
+            bodyColor.src = "../assets/img/avatarCollection/bodyColor" + this.customAvatar.bodyColor + ".png";
+
+            let face = new Image();
+            face.src = "../assets/img/avatarCollection/face" + this.customAvatar.face + ".png";
+
+            let shoes = new Image();
+            shoes.src = "../assets/img/avatarCollection/shoes" + this.customAvatar.shoes + ".png";
+
+            let lineArt = new Image();
+            lineArt.src = "../assets/img/avatarCollection/lineArtObligatory.png";
+
+
+            ctx.drawImage(bodyColor, 0, 0);
+            ctx.drawImage(face, 0, 0);
+            ctx.drawImage(shoes, 0, 0);
+            ctx.drawImage(bodyImg, 0, 0);
+            ctx.drawImage(lineArt, 0, 0);
+            ctx.drawImage(headImg, 0, 0);
+        },
+
+
+
+
+    },
+    computed: {
+
+    },
+}).mount('#app')
