ac36118ab4dcfa11a92dd243f612f07ba07874a0
AdrianoBalbo <adriano.balbo00@gmail.com>
2022-09-15 14:06:37 -0300

    Avanzando en el index.htmml, metiendo "estrellas" y trabajando en responsiveness

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -4,6 +4,8 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            isDown:Boolean,
+            
         }
     },
     created() {
@@ -13,10 +15,44 @@ createApp({
     },
     methods: {
 
-        
+
     },
     computed: {
 
     },
 }).mount('#app')
 
+const slider = document.getElementById("slider");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+    console.log(walk);
+});
+
+window.addEventListener("wheel", (e) => {
+    if (e.deltaY > 0) {
+        slider.scrollLeft += 100
+    }else{
+     slider.scrollLeft -= 100}
+  });
