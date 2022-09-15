0183cff1a71b0d019f4ba6fc9c0fb1c6499da03a
Adrian Rojas <rojasadrianezequiel@gmail.com>
2022-09-15 18:51:46 -0300

    empezamos de 0,dia 2, 19.00 (juega estudiantes) carpeta models

--- src/main/java/com/mindhub/skywalkies/SkywalkiesApplication.java
+++ src/main/java/com/mindhub/skywalkies/SkywalkiesApplication.java
@@ -1,11 +1,12 @@
 package com.mindhub.skywalkies;
 
-
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 
 @SpringBootApplication
@@ -14,7 +15,7 @@ public class SkywalkiesApplication {
 		SpringApplication.run(SkywalkiesApplication.class, args);
 	}
 	@Bean
-public CommandLineRunner initData() {
+public CommandLineRunner initData(ClientRespository clientRespository, ShoeRepository shoeRepository, AvatarRepository avatarRepository) {
 		return (args) -> {
 
 		};
