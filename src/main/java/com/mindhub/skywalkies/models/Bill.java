bde2923bdf27c2a2c6c7c71cc28164ce1c9b1725
Tomas Grunfeld <dubstergamer@gmail.com>
2022-09-15 19:36:42 -0300

    Clase Producto 19:36

--- src/main/java/com/mindhub/skywalkies/models/Bill.java
+++ src/main/java/com/mindhub/skywalkies/models/Bill.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+
 public class Bill {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
--- src/main/java/com/mindhub/skywalkies/models/Product.java
+++ src/main/java/com/mindhub/skywalkies/models/Product.java
@@ -1,4 +1,92 @@
 package com.mindhub.skywalkies.models;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+
+    private String name, color, type;
+
+    private Boolean active;
+
+    private int size;
+
+    private int stock;
+
+    public Product() {
+    }
+
+    public Product(String name, String color, String type, Boolean active, int size, int stock) {
+        this.name = name;
+        this.color = color;
+        this.type = type;
+        this.active = active;
+        this.size = size;
+        this.stock = stock;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
