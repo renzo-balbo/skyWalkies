5aec4a9576fa1b12aac850b852682960fd0d31f8
Adrian Rojas <rojasadrianezequiel@gmail.com>
2022-09-15 19:10:34 -0300

    empezamos de 0,dia 2, 19.00 (juega estudiantes) carpeta models

--- src/main/java/com/mindhub/skywalkies/repositories/ClientRepository.java
+++ src/main/java/com/mindhub/skywalkies/repositories/ClientRepository.java
@@ -1,4 +1,10 @@
 package com.mindhub.skywalkies.repositories;
 
-public class ClientRepository {
+import ch.qos.logback.core.net.server.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
 }
