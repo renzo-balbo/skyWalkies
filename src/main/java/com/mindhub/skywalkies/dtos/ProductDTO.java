3800c6c76d462eb314e3a0f56ad67f84b83bcdb2
Tomas Grunfeld <dubstergamer@gmail.com>
2022-09-15 19:43:15 -0300

    DTO Y REPO Producto 19:43

--- src/main/java/com/mindhub/skywalkies/dtos/ProductDTO.java
+++ src/main/java/com/mindhub/skywalkies/dtos/ProductDTO.java
@@ -1,4 +1,50 @@
 package com.mindhub.skywalkies.dtos;
 
+import com.mindhub.skywalkies.models.Product;
+
 public class ProductDTO {
+
+    private String name, color, type;
+
+    private Boolean active;
+
+    private int size;
+
+    private int stock;
+
+    public ProductDTO(Product product) {
+        this.name = product.getName();
+        this.active = product.getActive();
+        this.size = product.getSize();
+        this.color = product.getColor();
+        this.stock = product.getStock();
+        this.type = product.getType();
+
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getStock() {
+        return stock;
+    }
 }
--- src/main/java/com/mindhub/skywalkies/repositories/ProductRepository.java
+++ src/main/java/com/mindhub/skywalkies/repositories/ProductRepository.java
@@ -1,4 +1,13 @@
 package com.mindhub.skywalkies.repositories;
 
-public interface ProductRepository {
+import com.mindhub.skywalkies.models.Bill;
+import com.mindhub.skywalkies.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
+
+
