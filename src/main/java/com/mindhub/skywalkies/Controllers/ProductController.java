c17a2c7088ba68feb26180551fc532d81d3f11ae
Adrian Rojas <rojasadrianezequiel@gmail.com>
2022-09-19 14:32:56 -0300

    product controller para probar

--- src/main/java/com/mindhub/skywalkies/Controllers/ProductController.java
+++ src/main/java/com/mindhub/skywalkies/Controllers/ProductController.java
@@ -3,16 +3,18 @@ package com.mindhub.skywalkies.Controllers;
 import com.mindhub.skywalkies.Service.BillService;
 import com.mindhub.skywalkies.Service.ClientService;
 import com.mindhub.skywalkies.Service.ProductService;
+import com.mindhub.skywalkies.dtos.AddProductDTO;
 import com.mindhub.skywalkies.dtos.NewProductDTO;
 import com.mindhub.skywalkies.dtos.ProductDTO;
-import com.mindhub.skywalkies.models.Client;
-import com.mindhub.skywalkies.models.Product;
+import com.mindhub.skywalkies.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,43 +29,73 @@ public class ProductController {
 
     @Autowired
     private BillService billService;
+
     @GetMapping("/products")
-    public List<ProductDTO> getAllProducts(){
+    public List<ProductDTO> getAllProducts() {
         return productService.getAllProducts().stream().map(ProductDTO::new).collect(Collectors.toList());
     }
+
     @GetMapping("/products/{id}")
-    public  ProductDTO getProduct(@PathVariable long id){return new ProductDTO(productService.getProductById(id));}
+    public ProductDTO getProduct(@PathVariable long id) {
+        return new ProductDTO(productService.getProductById(id));
+    }
 
     @PostMapping("/products/add")
-    public ResponseEntity<Object> newProduct(@RequestBody NewProductDTO NewProductDTO, Authentication authentication){
-            Client client = clientService.findClientByEmail(authentication.getName());
+    public ResponseEntity<Object> newProduct(@RequestBody NewProductDTO NewProductDTO, Authentication authentication) {
+        Client client = clientService.findClientByEmail(authentication.getName());
 //        Product productName = productService.findByName(AddProductDTO.getName());
-        if (NewProductDTO.getSizes().isEmpty() || NewProductDTO.getPrice() < 0 || NewProductDTO.getShoeColors().isEmpty() || NewProductDTO.getType().isEmpty() || NewProductDTO.getName().isEmpty()){
+        if (NewProductDTO.getSizes().isEmpty() || NewProductDTO.getPrice() < 0 || NewProductDTO.getShoeColors().isEmpty() || NewProductDTO.getType().isEmpty() || NewProductDTO.getName().isEmpty()) {
             return new ResponseEntity<>("Please fill all the fields", HttpStatus.FORBIDDEN);
-        }
-        else {
+        } else {
             Product productPrueba1 = new Product(NewProductDTO.getName(), NewProductDTO.getShoeColors(), NewProductDTO.getType(), NewProductDTO.getActive(), NewProductDTO.getSizes(), NewProductDTO.getStock(), NewProductDTO.getPrice());
             productService.saveProduct(productPrueba1);
             return new ResponseEntity<>("Product loaded successfully", HttpStatus.CREATED);
         }
     }
+
     @PatchMapping("/products/edit")
-    public  ResponseEntity<Object> editProduct(@RequestBody ProductDTO productDTO, Authentication authentication){
+    public ResponseEntity<Object> editProduct(@RequestBody ProductDTO productDTO, Authentication authentication) {
         Client client = clientService.findClientByEmail(authentication.getName());
         Product product = productService.getProductById(productDTO.getId());
-        if (productDTO.getName().isEmpty() || productDTO.getType().isEmpty() || productDTO.getShoeColors().isEmpty() || productDTO.getSizes().isEmpty() || productDTO.getStock() <0 || productDTO.getPrice()<0){
-            return new ResponseEntity<>("no jaja",HttpStatus.FORBIDDEN);
+        if (productDTO.getName().isEmpty() || productDTO.getType().isEmpty() || productDTO.getShoeColors().isEmpty() || productDTO.getSizes().isEmpty() || productDTO.getStock() < 0 || productDTO.getPrice() < 0) {
+            return new ResponseEntity<>("no jaja", HttpStatus.FORBIDDEN);
         } else {
-        product.setName(productDTO.getName());
-        product.setType(productDTO.getType());
-        product.setShoeColors(productDTO.getShoeColors());
-        product.setSize(productDTO.getSizes());
-        product.setStock(productDTO.getStock());
-        product.setPrice(productDTO.getPrice());
+            product.setName(productDTO.getName());
+            product.setType(productDTO.getType());
+            product.setShoeColors(productDTO.getShoeColors());
+            product.setSize(productDTO.getSizes());
+            product.setStock(productDTO.getStock());
+            product.setPrice(productDTO.getPrice());
 
-        productService.saveProduct(product);
-        return  new ResponseEntity<>("si jejox",HttpStatus.ACCEPTED);
+            productService.saveProduct(product);
+            return new ResponseEntity<>("si jejox", HttpStatus.ACCEPTED);
         }
     }
 
+    @Transactional
+    @PostMapping("/products/addCart")
+    public ResponseEntity<Object> addProduct(@RequestBody AddProductDTO addProductDTO, Authentication authentication) {
+        Client client = clientService.findClientByEmail(authentication.getName());
+        Product product = productService.getProductById(addProductDTO.getId());
+
+        if (addProductDTO.getProductName().isEmpty() || addProductDTO.getQuantity() < product.getStock() || !addProductDTO.getSize().equals(product.getSize())) {
+            return new ResponseEntity<>("no jaja", HttpStatus.FORBIDDEN);
+        }
+
+        if(client.getBills().stream().filter(bill -> bill.isPayed()==false).count()>0){
+            Bill bill = client.getBills().stream().findFirst(billService)
+        }
+
+
+
+            Bill bill = new Bill(LocalDateTime.now(), false, 0);
+            Client_order client_order = new Client_order(bill);
+            Ordered_product ordered_product = new Ordered_product(client_order, addProductDTO.getQuantity(), addProductDTO.getSize(), ,product);
+
+    }
+
+    public void priceXquantity(Integer price, Integer quantity){
+
+
+    }
 }
