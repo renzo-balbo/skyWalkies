52c0ba89f81e6f800333fb984174e96bfc368057
Tomas Grunfeld <dubstergamer@gmail.com>
2022-09-20 12:17:15 -0300

    Pdf On

---
+++ src/main/java/com/mindhub/skywalkies/Service/PDFService.java
@@ -0,0 +1,15 @@
+package com.mindhub.skywalkies.Service;
+
+import com.itextpdf.text.DocumentException;
+import com.mindhub.skywalkies.dtos.PDFParamsDTO;
+import com.mindhub.skywalkies.models.Bill;
+import com.mindhub.skywalkies.models.Client_order;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public interface PDFService {
+    public void generatePDF(HttpServletResponse response, List<Client_order> client_orders, Bill bill);
+}
